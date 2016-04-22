f23dece9138f54d86671a0288068764c8579d18e
Ivan Ozerian <vio1990@ukr.net>
2016-04-22 18:26:10 +0300

    not important changes

--- src/main/java/com/ozerian/java/app/Bootstrap.java
+++ src/main/java/com/ozerian/java/app/Bootstrap.java
@@ -1,10 +1,13 @@
 package com.ozerian.java.app;
 
 
+import com.ozerian.lib.calculator.exceptions.IncorrectInputDataException;
+import com.ozerian.lib.calculator.exceptions.WrongInputDataException;
 import com.ozerian.lib.calculator.model.Calculator;
 
 public class Bootstrap {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws WrongInputDataException, IncorrectInputDataException {
         Calculator calculator = new Calculator();
+
     }
 }
