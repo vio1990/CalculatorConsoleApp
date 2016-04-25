61ed012ff77471a65b892a33fb6a5ef1dbe9ce23
Ivan Ozerian <vio1990@ukr.net>
2016-04-25 18:48:25 +0300

    Library change

--- src/main/java/com/ozerian/java/app/Bootstrap.java
+++ src/main/java/com/ozerian/java/app/Bootstrap.java
@@ -1,13 +1,22 @@
 package com.ozerian.java.app;
 
-
 import com.ozerian.lib.calculator.exceptions.IncorrectInputDataException;
+import com.ozerian.lib.calculator.exceptions.NotSupportedException;
 import com.ozerian.lib.calculator.exceptions.WrongInputDataException;
 import com.ozerian.lib.calculator.model.Calculator;
 
 public class Bootstrap {
-    public static void main(String[] args) throws WrongInputDataException, IncorrectInputDataException {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
-
+        try {
+            String result = calculator.calculatorExecute("15+13");
+            System.out.println(result);
+        } catch (WrongInputDataException e) {
+            e.printStackTrace();
+        } catch (IncorrectInputDataException e) {
+            e.printStackTrace();
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/ozerian/java/app/BootstrapTest.java
+++ src/test/java/com/ozerian/java/app/BootstrapTest.java
@@ -1,7 +1,10 @@
 package com.ozerian.java.app;
 
 
+import com.ozerian.lib.calculator.model.Calculator;
+
 public class BootstrapTest {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
     }
 }
