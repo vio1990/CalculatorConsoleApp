b8488d3bfa08b48f7686a5ec6b94fc3191ea2914
Ivan Ozerian <vio1990@ukr.net>
2016-04-26 15:58:16 +0300

    New Library Add. Working test

--- src/main/java/com/ozerian/java/app/Bootstrap.java
+++ src/main/java/com/ozerian/java/app/Bootstrap.java
@@ -1,14 +1,19 @@
 package com.ozerian.java.app;
 
 
+import com.ozerian.java.app.optional_math_operations.NumberDivision;
+import com.ozerian.java.app.util.InputDataReader;
 import com.ozerian.lib.calculator.exceptions.IncorrectInputDataException;
 import com.ozerian.lib.calculator.exceptions.NotSupportedException;
 import com.ozerian.lib.calculator.exceptions.WrongInputDataException;
 import com.ozerian.lib.calculator.model.Calculator;
 
 public class Bootstrap {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IncorrectInputDataException, NotSupportedException, WrongInputDataException {
         Calculator calculator = new Calculator();
-
+        InputDataReader reader = new InputDataReader();
+        String expression = reader.inputReader();
+        String result = calculator.calculatorExecute(expression);
+        System.out.println(result);
     }
 }
