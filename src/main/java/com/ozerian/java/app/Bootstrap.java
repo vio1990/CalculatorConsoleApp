35b3b3aeef4d7cc91ca012f77df1cf37dcb72f8b
Ivan Ozerian <vio1990@ukr.net>
2016-04-26 16:01:26 +0300

    Addition new operations.

--- src/main/java/com/ozerian/java/app/Bootstrap.java
+++ src/main/java/com/ozerian/java/app/Bootstrap.java
@@ -2,6 +2,7 @@ package com.ozerian.java.app;
 
 
 import com.ozerian.java.app.optional_math_operations.NumberDivision;
+import com.ozerian.java.app.optional_math_operations.NumberMultiplication;
 import com.ozerian.java.app.util.InputDataReader;
 import com.ozerian.lib.calculator.exceptions.IncorrectInputDataException;
 import com.ozerian.lib.calculator.exceptions.NotSupportedException;
@@ -12,6 +13,8 @@ public class Bootstrap {
     public static void main(String[] args) throws IncorrectInputDataException, NotSupportedException, WrongInputDataException {
         Calculator calculator = new Calculator();
         InputDataReader reader = new InputDataReader();
+        new NumberDivision(calculator.getParser(), calculator.getSupportedOperation());
+        new NumberMultiplication(calculator.getParser(), calculator.getSupportedOperation());
         String expression = reader.inputReader();
         String result = calculator.calculatorExecute(expression);
         System.out.println(result);
