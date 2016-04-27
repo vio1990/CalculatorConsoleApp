7e93060aef08496d5cfb89390c691e2b253f4a29
Ivan Ozerian <vio1990@ukr.net>
2016-04-27 12:36:17 +0300

    Example.

--- src/main/java/com/ozerian/java/app/Bootstrap.java
+++ src/main/java/com/ozerian/java/app/Bootstrap.java
@@ -3,20 +3,28 @@ package com.ozerian.java.app;
 
 import com.ozerian.java.app.optional_math_operations.NumberDivision;
 import com.ozerian.java.app.optional_math_operations.NumberMultiplication;
-import com.ozerian.java.app.util.InputDataReader;
 import com.ozerian.lib.calculator.exceptions.IncorrectInputDataException;
 import com.ozerian.lib.calculator.exceptions.NotSupportedException;
 import com.ozerian.lib.calculator.exceptions.WrongInputDataException;
 import com.ozerian.lib.calculator.model.Calculator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Bootstrap {
-    public static void main(String[] args) throws IncorrectInputDataException, NotSupportedException, WrongInputDataException {
+    public static void main(String[] args)  {
+       /* ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");*/
         Calculator calculator = new Calculator();
-        InputDataReader reader = new InputDataReader();
         new NumberDivision(calculator.getParser(), calculator.getSupportedOperation());
-        new NumberMultiplication(calculator.getParser(), calculator.getSupportedOperation());
-        String expression = reader.inputReader();
-        String result = calculator.calculatorExecute(expression);
-        System.out.println(result);
+        new NumberMultiplication(calculator.getParser(),calculator.getSupportedOperation());
+        try {
+            calculator.calculatorExecute("5*3");
+        } catch (WrongInputDataException e) {
+            e.printStackTrace();
+        } catch (IncorrectInputDataException e) {
+            e.printStackTrace();
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
