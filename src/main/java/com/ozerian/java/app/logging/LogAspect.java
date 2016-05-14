8466cf278cdccc1259cf29ca40337103c7039d8c
Ivan Ozerian <vio1990@ukr.net>
2016-05-14 19:51:21 +0300

    Addition of logging.

--- src/main/java/com/ozerian/java/app/logging/LogAspect.java
+++ src/main/java/com/ozerian/java/app/logging/LogAspect.java
@@ -1,14 +1,15 @@
 package com.ozerian.java.app.logging;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class LogAspect {
 
-    static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+    private static final Logger logger = Logger.getLogger(LogAspect.class);
 
     public Object methodsLog(ProceedingJoinPoint pjp) throws Throwable {
+        PropertyConfigurator.configure("D:\\GoJavaOnlineHomeworks\\CalculatorApp\\src\\main\\resources\\log4j.properties");
         logger.info("Method " + pjp.getSignature().getName() + " is running");
         Object result = pjp.proceed();
         logger.info("Method " + pjp.getSignature().getName() + " stop working");
